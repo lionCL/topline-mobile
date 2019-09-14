f4950b373ac4fbe543caf3203ea9b595dbc20157
lionCL <18688788437@163.com>
2019-09-15 01:49:52 +0800

    封装token操作为单独文件. 并完成登录时将token存入store中

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+//引入token操作封装文件
+import * as author from '@/utils/author'
+
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    userInfo: ''
+    userInfo: author.getAuthor()
   },
   mutations: {
     //设置用户信息
-    setUserInfo(state, userInfo) {}
+    setUserInfo(state, userInfo) {
+      author.setAuthor(userInfo)
+    }
   },
   actions: {}
 })
---
+++ src/utils/author.js
@@ -0,0 +1,22 @@
+//对token操作的封装
+
+//定义键
+const token = 'user'
+
+//token的保存
+function setAuthor(value) {
+  window.localStorage.setItem(token, JSON.stringify(value))
+}
+
+//token的获取
+function getAuthor(value) {
+  return window.localStorage.getItem(token) ? JSON.parse(window.localStorage.getItem(token)) : null
+}
+
+//token的清除
+function removeAuthor(value) {
+  window.localStorage.removeItem(token)
+}
+
+//暴露接口
+export { setAuthor, getAuthor, removeAuthor }
