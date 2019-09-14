b82d62ae07dab8d5c6682cf19f759122c8e7bb2d
lionCL <18688788437@163.com>
2019-09-15 01:03:16 +0800

    validate验证插件的检验功能的完成
    基本使用  添加其他语法提示(中文)  添加自定义验证提示语  添加自定义检验触发事件

--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 //导入抽离路由文件
 import router from '@/router'
+
 //导入vuex文件
 import store from '@/store'
+
 //导入vant组件
 import Vant from 'vant'
 import 'vant/lib/index.css'
 Vue.use(Vant)
 
-Vue.config.productionTip = false
+//导入validate验证插件 / 语法模块
+import VeeValidate, { Validator } from 'vee-validate'
+//导入validate语言包
+import zh_CN from 'vee-validate/dist/locale/zh_CN'
+//使用语音包
+Validator.localize('zh_CN', zh_CN)
+//使用validate
+Vue.use(VeeValidate, {
+  events: '' //自定义校验事件,禁用默认的
+})
 
+Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
   //挂在路由
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,18 +9,25 @@
                  required
                  clearable
                  label="手机号码:"
-                 placeholder="请输入手机号" />
+                 placeholder="请输入手机号"
+                 v-validate="'digits:11'"
+                 name="mobile"
+                 :error-message="errors.first('mobile')" />
       <van-field v-model="code"
                  type="text"
                  left-icon='browsing-history-o'
                  label="验证码:"
                  placeholder="请输入验证码"
-                 required />
+                 required
+                 v-validate="'required'"
+                 name="code"
+                 :error-message="errors.first('code')" />
     </van-cell-group>
     <!-- 登录按钮 -->
     <div class="mybtn">
       <van-button type="info"
-                  size="large">登录</van-button>
+                  size="large"
+                  @click="doLogin">登录</van-button>
     </div>
   </div>
 </template>
@@ -30,9 +37,42 @@ export default {
   name: 'login',
   data() {
     return {
-      mobile: '188888888888',
+      mobile: '18888888888',
       code: '246810'
     }
+  },
+  methods: {
+    //设置自己的验证规则
+    setRules() {
+      //验证对象
+      const dict = {
+        //自定义验证规则提示信息
+        custom: {
+          mobile: {
+            digits: () => '请输入11位手机号码'
+          },
+          code: {
+            required: () => '请输入验证码'
+          }
+        }
+      }
+      //使用校验规则
+      this.$validator.localize('zh_CN', dict)
+    },
+
+    doLogin() {
+      //所有校验成功后的逻辑代码
+      this.$validator.validate().then(valid => {
+        if (valid) {
+          //校验成功的逻辑
+          console.log(valid)
+        }
+      })
+    }
+  },
+  mounted() {
+    //使用规则
+    this.setRules()
   }
 }
 </script>
