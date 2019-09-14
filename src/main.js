4c0ddfec2d04eaf6db886023ebf984274082160a
lionCL <18688788437@163.com>
2019-09-14 23:53:27 +0800

    项目准备-创建分离store文件

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 //导入抽离路由文件
 import router from '@/router'
+//导入vuex文件
+import store from '@/store'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   //挂在路由
-  router
+  router,
+  //挂在vuex
+  store
 }).$mount('#app')
