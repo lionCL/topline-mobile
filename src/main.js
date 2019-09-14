992de42743a297d15d657f8bad933c334715876c
lionCL <18688788437@163.com>
2019-09-14 23:47:19 +0800

    项目准备
    删除多余文件.  抽离路由文件.挂在路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入抽离路由文件
+import router from '@/router'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //挂在路由
+  router
 }).$mount('#app')
