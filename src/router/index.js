6eb1dc1962794da4d771e931f5569dbb76b5a2e6
lionCL <18688788437@163.com>
2019-09-15 00:02:38 +0800

    项目目录结构准备完毕
    安装less-loader  以及登录页路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+//导入组件
+const Login = () => import('@/views/login/')
 
 Vue.use(VueRouter)
 
-const routes = [{ path: '/', redirect: '/login' }]
+const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login, name: 'login' }
+]
 
 const router = new VueRouter({
   routes
